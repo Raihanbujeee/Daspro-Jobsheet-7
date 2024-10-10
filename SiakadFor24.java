67c0a1bff8bad69b495fba339200817c39382a4c
Raihanbujeee <bujexixi@gmail.com>
2024-10-11 03:12:15 +0700

    modilulus

--- SiakadFor24.java
+++ SiakadFor24.java
@@ -1,11 +1,37 @@
-import java.util.Scanner;
-public class SiakadFor24 {
+// import java.util.Scanner;
+// public class SiakadFor24 {
 
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in); 
+//         double nilai, tertinggi = 100, terendah = 0;
+
+//         for(int i = 1; i <= 10; i++) {
+//             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
+//             nilai = input.nextDouble();
+//             if (nilai > tertinggi) {
+//                 tertinggi = nilai;
+//             }
+//             if (nilai < terendah) {
+//                 terendah = nilai;
+//             }
+//         }
+//         System.out.println("Nilai tertinggi = " + tertinggi);
+//         System.out.println("Nilai terendah = " + terendah);
+
+//     }
+// }
+
+
+import java.util.Scanner;
+
+public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); 
         double nilai, tertinggi = 0, terendah = 100;
-
-        for(int i = 1; i <= 10; i++) {
+        int lulus = 0, tidakLulus = 0; 
+        int batasKelulusan = 60;
+       
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = input.nextDouble();
             if (nilai > tertinggi) {
@@ -14,9 +40,18 @@ public class SiakadFor24 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+           
+            if (nilai >= batasKelulusan) {
+                lulus++;
+            } else {
+                tidakLulus++; 
+            }
         }
         System.out.println("Nilai tertinggi = " + tertinggi);
         System.out.println("Nilai terendah = " + terendah);
-        
+
+        System.out.println("Jumlah mahasiswa yang lulus = " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus = " + tidakLulus);
     }
 }
+
